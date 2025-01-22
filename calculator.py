217536a24c78cbf5ec5f83987c0793735cb8e50a
Urvashi Jawere <urvashijawere@Urvashis-MacBook-Pro.local>
2025-01-22 15:44:46 +0530

    003: Negative number handling, delimiter handling

--- calculator.py
+++ calculator.py
@@ -1,4 +1,8 @@
 class Calculator:
+    def __init__(self, delimiter):
+        # assumption: test case inputs will adhere to delimiter of the object
+        self.delimiter = delimiter
+
     def add(self, numbers):
         """ numbers is a string, extract items from string and return their addition"""
         if len(numbers) == 0:
@@ -7,9 +11,13 @@ class Calculator:
         if numbers.endswith('\n'):
             return "invalid"
 
-        sum = 0
-        for item in numbers:
-            if item.isdigit():
-                sum += int(item)
+        new_numbers = numbers.replace('\n', ',').split(self.delimiter)
+        negatives = [x for x in new_numbers if int(x) < 0]
+        if len(negatives) > 0:
+            neg_numbers = ",".join(negatives)
+            raise ValueError("Negative numbers not allowed: {}".format(neg_numbers))
 
-        return sum
+        total = 0
+        for item in new_numbers:
+            total += int(item)
+        return total
--- test_calculator.py
+++ test_calculator.py
@@ -5,12 +5,14 @@ from calculator import Calculator
 class TestCalculator(unittest.TestCase):
     def setUp(self):
         """Create a Calculator instance for testing."""
-        self.calc = Calculator()
+        comma_delimiter = ","
+        semicolon_delimiter = ";"
+        self.calc = Calculator(comma_delimiter)
+        self.calc2 = Calculator(semicolon_delimiter)
 
     def test_add_empty_string(self):
         """Test addition."""
         self.assertEqual(self.calc.add(""), 0)
-
 
     def test_add_numbers_in_string(self):
         self.assertEqual(self.calc.add("1"), 1)
@@ -21,5 +23,18 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add("1\n2,3"), 6)
         # new line at the end is invalid
         self.assertEqual(self.calc.add("1,\n"), "invalid")
+
+    def test_negative_numbers(self):
+        with self.assertRaises(ValueError) as context:
+            self.calc.add("1,2,3,-4,-5")
+        self.assertEqual(str(context.exception), "Negative numbers not allowed: -4,-5")
+
+        with self.assertRaises(ValueError) as context:
+            self.calc2.add("1;2;3;-4;-5")
+        self.assertEqual(str(context.exception), "Negative numbers not allowed: -4,-5")
+
+    def test_different_delimiter(self):
+        self.assertEqual(self.calc2.add("1;2"), 3)
+
 
 
